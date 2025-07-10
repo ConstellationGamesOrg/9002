eb737c581cf79002481e906ce7a94cacc1b14a25
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2025-07-10 16:25:31 -0400

    Removed logger.hpp as we are using spdlog, and removed global.hpp. (Use precompiled header instead?)

--- src/global.hpp
+++
@@ -1,7 +0,0 @@
-// Global header imported by all files in the project
-
-#pragma once
-
-// Maybe do platform checking here, else returning an error
-
-// Define some universally useful custom types here (or shorthands)
