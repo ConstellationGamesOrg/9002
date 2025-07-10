dd02011c117da600720e93942066e5383aa38717
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2025-07-10 18:11:40 -0400

    Temporary doxygen documentation test.

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+ /**
+  * @class Game
+  * @brief The Game class.
+  *
+  * Does game stuff.
+  *
+  */
 class Game
 {
 public:
 	Game();
 	virtual ~Game();
 
+	/**
+	 * @brief Short description.
+	 * @details Longer detailed description.
+	 *
+	 * Blah blah example documentation.
+	 *
+	 * @param x Example param x.
+	 * @param y Example param y.
+	 * @note Example note.
+	 * @returns void.
+	 * @throws Nothing.
+	 *
+	 */
 	void Run();
 
 private:
